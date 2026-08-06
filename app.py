d4569e6adc15afa27263401653acb4498182cae1
genrane69-web <genrane69@gmail.com>
2026-08-07 03:42:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import requests
 # -----------------------------------------------------------------------------
 st.set_page_config(
     page_title="AppRental - ระบบเว็บแอปสำหรับธุรกิจ",
-    page_icon="🪄",
+    page_icon=" ",
     layout="wide",
     initial_sidebar_state="collapsed"
 )
